e6653846ffaa55a7d0e2536bbe3d59850d0586df
MaorUziel <63524728+MaorUziel@users.noreply.github.com>
2021-09-16 11:51:50 +0300

    Update test_run.py

--- test_run.py
+++ test_run.py
@@ -15,11 +15,11 @@ gmail_service = auth.get_service_gmail()
 
 if Setup.WEEKLY_TEST == 'True' or Setup.WEEKLY_TEST == 'true':
     week_session_failure_dict = test_analytics.get_session_decreased_failure(analytics_service,
-                                                                             VIEW_DICT, start=30, end=0)
-    test_analytics.send_email_on_session_decreased_failure(gmail_service, week_session_failure_dict, 30)
+                                                                             VIEW_DICT, start=7, end=0)
+    test_analytics.send_email_on_session_decreased_failure(gmail_service, week_session_failure_dict, 7)
 
-    week_sign_ups_failure_dict = test_analytics.get_challenges_sign_ups_failure(analytics_service, start=30, end=0)
-    test_analytics.send_email_on_sign_ups_decreased_failure(gmail_service, week_sign_ups_failure_dict, 30)
+    week_sign_ups_failure_dict = test_analytics.get_challenges_sign_ups_failure(analytics_service, start=7, end=0)
+    test_analytics.send_email_on_sign_ups_decreased_failure(gmail_service, week_sign_ups_failure_dict, 7)
 
 if Setup.MONTHLY_TEST == 'True' or Setup.MONTHLY_TEST == 'true':
     week_session_failure_dict = test_analytics.get_session_decreased_failure(analytics_service,
