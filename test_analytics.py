079ff2504b92c8302c9c28331bbb8a1dc1c156f0
MaorUziel <63524728+MaorUziel@users.noreply.github.com>
2021-10-20 12:58:17 +0300

    Update test_analytics.py

--- test_analytics.py
+++ test_analytics.py
@@ -25,7 +25,7 @@ SIGN_UPS_DECREASE_FAILURE = -25
 DONT_TEST_CHALLENGE_TYPE = ['Challenge22 - Hebrew Form Plugin', '(not set)', 'Challenge22 - Hebrew P3']
 ### Email Const ###
 SENDER = "me"
-TO_LIST = ["dev@animals-now.org", "maor@animals-now.org", "saharr@animals-now.org"]
+TO_LIST = ["dev@animals-now.org", "maor@animals-now.org", "saharr@animals-now.org", "roni@animals-now.org"]
 USER_ID = "me"
 
 
